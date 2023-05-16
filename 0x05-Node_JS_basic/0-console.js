6b178ec431567e99128f5b9c58b4202cb08ae32b
zeezatcoder <azeezatmaja@gmail.com>
2023-05-16 09:43:40 +0000

    task complete

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Display messages on the STDOUT.
+ * @param {String} msg message to display.
+ * @author Azeezat Maja <https://github.com/zeezatcoder>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
